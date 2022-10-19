4bd6fa8d20e89752057b0999405fbdf4f2758610
roseInWeb <i.a.semyonova@mail.ru>
2022-10-19 12:10:36 +0400

    feat: made it possible to calculate with brackets

--- src/index.js
+++ src/index.js
@@ -14,14 +14,15 @@ const calcBtnMm = document.querySelector('.calc-btn-mm');
 const calcBtnMs = document.querySelector('.calc-btn-ms');
 const calcBtnMr = document.querySelector('.calc-btn-mr');
 const calcBtnMc = document.querySelector('.calc-btn-mc');
+const calcBtnBra = document.querySelectorAll('.calc-btn-bra');
 
-// calcInput.addEventListener('click', () => {
-//     calcInput.blur();
-// })
+calcInput.addEventListener('click', () => {
+    calcInput.blur();
+})
 
 const check = (str) => {
     let calcInputStr = calcInput.value.split('');
-    let cISLS = calcInputStr[calcInputStr.length - 1];
+    let cISLS = calcInputStr.at(-1);
     if (cISLS !== str && calcInputStr.length !== 0) {
         calcInput.value += str;
     }
@@ -38,24 +39,75 @@ calcBtnNum.forEach((el) => {
         if (calcInput.value == '0') {
             calcInput.value = '';
         }
-        calcInput.value += el.textContent;
+        if (calcInput.value.length <= 13) {
+            calcInput.value += el.textContent;
+        }
     });
 });
 
 calcBtnMul.addEventListener('click', () => {
-    check('*');
+    if (calcInput.value.length <= 13) {
+        check('*');
+    }
 });
 
 calcBtnDiv.addEventListener('click', () => {
-    check('/');
+    if (calcInput.value.length <= 13) {
+        check('/');
+    }
 });
 
 calcBtnPlus.addEventListener('click', () => {
-    check('+');
+    if (calcInput.value.length <= 13) {
+        check('+');
+    }
 });
 
 calcBtnMinus.addEventListener('click', () => {
-    check('-');
+    if (calcInput.value.length <= 13) {
+        check('-');
+    }
+});
+
+calcBtnBra[0].addEventListener('click', () => {
+    let calcInputStr = calcInput.value.split('');
+    let cISLS = calcInputStr.at(-1);
+    if (calcInputStr.length == 1 && calcInputStr.at(-1) == 0) {
+        calcInput.value = '';
+        calcInput.value += '(';
+    }
+    if (cISLS == '/' || cISLS == '*' || cISLS == '+' || cISLS == '-') {
+        calcInput.value += '(';
+    }
+});
+
+calcBtnBra[1].addEventListener('click', () => {
+    let calcInputStr = calcInput.value.split('');
+    let cISLS = calcInputStr.at(-1);
+    if (cISLS !== '/' && cISLS !== '*' && cISLS !== '+' && cISLS !== '-' && calcInputStr.length !== 1) {
+        let p = false;
+        for (let i = 0; i < calcInputStr.length; i++) {
+            if (calcInputStr[i] === '(') {
+                p = true;
+            }
+        }
+        if (p === true) {
+            calcInput.value += ')';
+        }
+    } else if (cISLS == '/' || cISLS == '*' || cISLS == '+' || cISLS == '-') {
+        let p = false;
+        for (let i = 0; i < calcInputStr.length; i++) {
+            if (calcInputStr[i] === '(') {
+                p = true;
+            }
+        }
+        if (p === true) {
+            cISLS = ')';
+            calcInputStr = calcInputStr.slice(0, calcInputStr.length - 1);
+            calcInput.value = calcInputStr.join('');
+            calcInput.value += cISLS;
+        }
+    }
 });
 
 calcBtnEr.addEventListener('click', () => {
@@ -73,9 +125,15 @@ calcBtnDel.addEventListener('click', () => {
 
 calcBtnEq.addEventListener('click', () => {
     let calcInputStr = calcInput.value;
+    let cISLS = calcInputStr.split('').at(-1);
+    if (cISLS == '/' || cISLS == '*' || cISLS == '+' || cISLS == '-') {
+        calcInputStr = calcInputStr.split('').slice(0, calcInputStr.length - 1);
+        calcInputStr = calcInputStr.join('');
+        calcInput.value = calcInputStr;
+    }
     try {
         if (eval(calcInputStr) !== Infinity) {
-            if (`${eval(calcInputStr)}`.length < 10) {
+            if (`${eval(calcInputStr)}`.length < 14) {
                 calcInput.value = eval(calcInputStr);
             } else {
                 let a = eval(calcInputStr);
@@ -87,7 +145,6 @@ calcBtnEq.addEventListener('click', () => {
         }
     } catch {
         alert('Erorr :(');
-        calcInput.value = 0;
     }
 });
 
